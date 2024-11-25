851455985918703d76130c99396247597a75e94b
TuananhDo0308 <TuananhDo0308@gmail.com>
2024-11-25 09:10:38 +0700

    ok

--- src/services/LinkAPI.ts
+++ src/services/LinkAPI.ts
@@ -2,4 +2,4 @@ const API_URL = "https://7903-2402-800-63a6-e30b-8182-1957-7895-a241.ngrok-free.
 export default API_URL;
 
 
-export const IMG_URL = " https://7903-2402-800-63a6-e30b-8182-1957-7895-a241.ngrok-free.app";
+export const IMG_URL = "https://7903-2402-800-63a6-e30b-8182-1957-7895-a241.ngrok-free.app";
